68771f3e07ec531861182aed72ad7e1ad9f2a683
swaroopkasaraneni <swaroopkasaraneni@gmail.com>
2025-04-05 13:16:41 +0530

    Update __init__.py

--- src/math_mcp_server_sse/__init__.py
+++ src/math_mcp_server_sse/__init__.py
@@ -1,17 +1,9 @@
-from .server import serve
-
+from .server import create_app
+import uvicorn
 
 def main():
-    """MCP Math Server - Maths functionality for MCP"""
-    import argparse
-    import asyncio
-
-    parser = argparse.ArgumentParser(
-        description="give a model the ability to make web requests"
-    )
-
-    asyncio.run(serve())
-
+    """MCP Math Server - SSE Transport Math Server"""
+    uvicorn.run(create_app(), host="0.0.0.0", port=5000)
 
 if __name__ == "__main__":
     main()
