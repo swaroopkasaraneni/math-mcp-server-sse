92ce44a3f0f3ac739a6eb65601ac14bb6e76f9f8
swaroopkasaraneni <swaroopkasaraneni@gmail.com>
2025-04-07 06:41:47 +0530

    Update __init__.py

--- src/math_mcp_server_sse/__init__.py
+++ src/math_mcp_server_sse/__init__.py
@@ -4,7 +4,7 @@ import asyncio
 def main():
     
     """MCP Math Server - SSE Transport Math Server"""
-    uvicorn.run("math_mcp_server_sse.server:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("math_mcp_server_sse.server:app", host="0.0.0.0", port=5000, reload=False)
 
 if __name__ == "__main__":
     main()
